a693a497007ceff25bcba0fb2b1088e5262b3a79
Yuanjie-Xia <54915190+Yuanjie-Xia@users.noreply.github.com>
2021-05-19 10:28:12 -0400

    Update fuzzFile.py

--- fuzzFile.py
+++ fuzzFile.py
@@ -376,6 +376,8 @@ def create_jmx_file(f_workload, loopingtime):
     temp = s[rng5]
     s[rng5] = s[rng6]
     s[rng6] = s[rng5]
+
+    s = s + s + s
     #if loopingtime==0:
     #   s = [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200]
     print(s)
