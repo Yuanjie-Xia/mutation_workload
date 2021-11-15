41149fc3bbfc77fd08916d66b523a74e0906a618
Yuanjie-Xia <54915190+Yuanjie-Xia@users.noreply.github.com>
2021-11-15 10:14:41 -0500

    update system command script

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import paramiko
+
 import mutate_workload_config
 import os
 from time import sleep
@@ -25,16 +27,24 @@ def main():
     workload.sort_workload()
     workload.generate_running_file()
     workload.set_config()
+    # send ratio.csv
 
     for loop_time in range(1, 100):
         os.system('sudo mv screenlog.0 screenlog' + str(loop_time) + '.0')
-        os.system('screen -d -m -L pidstat -p ALL -u -r -d -h -I -l ' + str(window_size))
         os.system('docker rm teastore-all')
-        os.system('docker run --cpus="' + workload.config[0] +'" -m=' + workload.config[1] +
+        os.system('docker run --cpus="' + workload.config[0] + '" -m=' + workload.config[1] +
                   'GB -e "DB_HOST=teastore-db" -p 8080:8080 -d --link teastore-db:teastore-db '
                   '--name teastore-all descartesresearch/teastore-all')
-        os.system("locust -f runtest1.py --headless --users 1 --spawn-rate 1 --run-time=30s -H http://localhost:8080")
-        os.system("locust -f runtest2.py --headless --users 1 --spawn-rate 1 --run-time=30s -H http://localhost:8080")
+        ssh_client = paramiko.SSHClient()
+        ssh_client.load_system_host_keys()
+        ssh_client.connect(hostname='10.128.0.35', username='yuanjiexia')
+        ssh_client.exec_command('scp ~/mutation_workload/ratio.csv yuanjiexia@10.128.0.45:~/mutation_workload')
+        sleep(30)
+        os.system('screen -d -m -L pidstat -p ALL -u -r -d -h -I -l ' + str(window_size))
+        ssh_client.exec_command("locust -f ~/mutation_workload/runtest1.py --headless --users 1 "
+                                "--spawn-rate 1 --run-time=30s -H http://localhost:8080")
+        ssh_client.exec_command("locust -f ~/mutation_workload/runtest2.py --headless --users 1 "
+                                "--spawn-rate 1 --run-time=30s -H http://localhost:8080")
         os.system('pkill screen')
         workload = mutate_workload_config.WorkLoad(window_size, 'TeaStore', logFileAddress=logFileAddress,
                                                    perfFileAddress=perfFileAddress,
@@ -45,6 +55,7 @@ def main():
         workload.evaluate_workload()
         workload.sort_workload()
         workload.generate_running_file()
+        # send ratio.csv
 
 
 if __name__ == "__main__":
