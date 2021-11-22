e0df3fb0a8f670a1f3c855dbcc7bebe9d2b02676
Yuanjie-Xia <54915190+Yuanjie-Xia@users.noreply.github.com>
2021-11-22 10:45:56 -0500

    update expor$source ~/.bash_profile

--- evaluate.py
+++ evaluate.py
@@ -69,6 +69,7 @@ def measure_d(workload_store, url_workload, config, loop_time):
             corr = list()
             for k in range(0, min(len(workload), len(workload_store))):
                 # distance = 1 - spatial.distance.cosine(workload_store[k:k + 1], workload[i:i + 1])
+                workload = workload.reset_index() # only use on a interupt continue
                 p_corr, _ = pearsonr(np.array(workload_store[k:k + 1])[0], np.array(workload[i:i + 1])[0])
                 distance = 1 - p_corr
                 corr.append(distance)
