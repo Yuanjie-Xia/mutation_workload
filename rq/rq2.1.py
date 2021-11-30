f35bc3b1824959198fcbd67abc6dbe7b174dc3d1
Yuanjie-Xia <54915190+Yuanjie-Xia@users.noreply.github.com>
2021-11-30 10:01:37 -0500

    Update rq2.1.py

--- rq/rq2.1.py
+++ rq/rq2.1.py
@@ -86,7 +86,7 @@ for j in tqdm(range(1, 6)):
         error = (result - test_set_y) / result
         sum.append(error)
 
-    for q in tqdm(range(len(training_set_b))):
+    for q in tqdm(range(int((j-1) * len(ori) / 5), int(j * len(ori) / 5))):
         # model.compile(loss="mse", optimizer="adam", metrics=["mape"])
         test_set_b = training_set_b.iloc[[q]]
         training_set_b_copy = training_set_b.copy()
